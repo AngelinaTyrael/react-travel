92fe97c87e09f011144ce8cc81f09c1e5d1bc11f
AngelinaTyrael <lucky18209714890@gmail.com>
2022-04-17 02:30:55 +0800

    主页内容:sparkles:

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from './App.module.css';
-import { Row, Col } from 'antd';
-
-import { Header, Footer, SideMenu, Carousel } from './components';
+import { Row, Col, Typography } from 'antd';
+import { Header, Footer, SideMenu, Carousel, ProductCollection } from './components';
+import { productList1, productList2, productList3 } from './mockups';
+import sideImage from './assets/images/sider_2019_12-09.png';
+import sideImage2 from './assets/images/sider_2019_02-04.png';
+import sideImage3 from './assets/images/sider_2019_02-04-2.png';
 
 function App() {
   return (
@@ -10,14 +13,50 @@ function App() {
       <Header />
       {/* 页面内容 content */}
       <div className={styles['page-content']}>
-        <Row style={{marginTop: 20}}>
+        <Row style={{ marginTop: 20 }}>
           <Col span={6}>
             <SideMenu />
           </Col>
           <Col span={18}>
             <Carousel />
-          </Col> 
+          </Col>
         </Row>
+        <ProductCollection
+          title={
+            <Typography.Title
+              level={3}
+              type='warning'
+            >
+              爆款推荐
+            </Typography.Title>
+          }
+          sideImage={sideImage}
+          products={productList1}
+        />
+        <ProductCollection
+          title={
+            <Typography.Title
+              level={3}
+              type='danger'
+            >
+              新品上市
+            </Typography.Title>
+          }
+          sideImage={sideImage2}
+          products={productList2}
+        />
+        <ProductCollection
+          title={
+            <Typography.Title
+              level={3}
+              type='success'
+            >
+              国内游推荐
+            </Typography.Title>
+          }
+          sideImage={sideImage3}
+          products={productList3}
+        />
       </div>
       <Footer />
     </div>
